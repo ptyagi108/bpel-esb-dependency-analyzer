97b176e7fa50a9424a5604dd63571dc74a40603d
tfrastia <tfrastia@aeca010c-e66b-6ba6-8b88-d3654008891e>
2010-05-13 08:52:28 +0000

    bpel-esb-dependency-analyzer: show activity icon in findUsage tree

--- trunk/bpel-esb-dependency-analyzer/src/main/java/com/tomecode/soa/bpel/dependency/analyzer/gui/panels/UsageTreePanel.java
+++ trunk/bpel-esb-dependency-analyzer/src/main/java/com/tomecode/soa/bpel/dependency/analyzer/gui/panels/UsageTreePanel.java
@@ -1,12 +1,18 @@
 package com.tomecode.soa.bpel.dependency.analyzer.gui.panels;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeCellRenderer;
 
 import com.tomecode.soa.bpel.dependency.analyzer.gui.tree.BasicTree;
 import com.tomecode.soa.bpel.dependency.analyzer.utils.FindUsage;
+import com.tomecode.soa.bpel.dependency.analyzer.utils.Usage;
 
 /**
  * 
@@ -17,16 +23,54 @@ public final class UsageTreePanel extends JPanel {
 
 	private static final long serialVersionUID = 2752156559028598059L;
 
-	private BasicTree basicTree;
+	private final BasicTree basicTree;
 
+	private final FindUsageTreeRenderer rendererTree;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param result
+	 */
 	public UsageTreePanel(FindUsage result) {
 		setLayout(new BorderLayout());
 		basicTree = new BasicTree();
+		rendererTree = new FindUsageTreeRenderer();
+		basicTree.setCellRenderer(rendererTree);
 		add(new JScrollPane(basicTree), BorderLayout.CENTER);
 		basicTree.addData(result);
 	}
 
+	/**
+	 * Set new data
+	 * 
+	 * @param result
+	 */
 	public final void addData(FindUsage result) {
 		basicTree.addData(result);
 	}
+
+	/**
+	 * 
+	 * @author Tomas Frastia
+	 * 
+	 */
+	private final class FindUsageTreeRenderer implements TreeCellRenderer {
+
+		@Override
+		public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+			DefaultTreeCellRenderer rnd = (DefaultTreeCellRenderer) new DefaultTreeCellRenderer().getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
+
+			if (value instanceof Usage) {
+				Usage usage = (Usage) value;
+				ImageIcon icon = usage.getActivity().getActivtyType().getImageIcon();
+				if (icon != null) {
+					rnd.setIcon(icon);
+				}
+			}
+
+			return rnd;
+		}
+	}
+
 }
